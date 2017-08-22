a316b6bd6802b6c933c8b8ab43d1b952006f9daf
Li . Jing <Li . Jing>
2017-08-22 14:44:55 +0800

    Initial commit

---
+++ app/src/main/java/com/lee/timeselector/thread/OnItemSelectedRunnable.java
@@ -0,0 +1,17 @@
+package com.lee.timeselector.thread;
+
+
+import com.lee.timeselector.view.WheelView;
+
+public final class OnItemSelectedRunnable implements Runnable {
+    final WheelView loopView;
+
+    public OnItemSelectedRunnable(WheelView loopview) {
+        loopView = loopview;
+    }
+
+    @Override
+    public final void run() {
+        loopView.onItemSelectedListener.onItemSelected(loopView.getCurrentItem());
+    }
+}
